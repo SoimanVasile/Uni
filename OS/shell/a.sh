a51a44ce916213c7c4f1c271ac7ffd130407b50c
SoimanVasile <vasilecristiansoiman@gmail.com>
2026-03-04 14:09:56 +0200

    OS

---
+++ OS/shell/a.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for A in  $*; do
+  if test -d $A; then
+    echo $A e director
+  elif test -f $A; then
+    echo $A e fisier
+  elif echo $A | grep -E -q "^[0-9]+$";then
+    echo $A e numar
+  else 
+    echo $A e altceva
+  fi
+done
+
+while read B; do
+  echo am citit $B
+done
